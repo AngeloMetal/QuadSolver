e6fcb9abcaa81e6aa9a18ad8c8cf813884f4b626
Angelo M <70480326+AngeloMetal@users.noreply.github.com>
2021-02-11 19:40:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace QuadSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.alpha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.vita = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // alpha
+            // 
+            this.alpha.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.alpha.Location = new System.Drawing.Point(81, 128);
+            this.alpha.Name = "alpha";
+            this.alpha.Size = new System.Drawing.Size(55, 31);
+            this.alpha.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "α";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(51, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "β";
+            // 
+            // vita
+            // 
+            this.vita.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.vita.Location = new System.Drawing.Point(81, 185);
+            this.vita.Name = "vita";
+            this.vita.Size = new System.Drawing.Size(55, 31);
+            this.vita.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(51, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "γ";
+            // 
+            // gama
+            // 
+            this.gama.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gama.Location = new System.Drawing.Point(81, 242);
+            this.gama.Name = "gama";
+            this.gama.Size = new System.Drawing.Size(55, 31);
+            this.gama.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(51, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quadratic equation solver";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(94, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "αx² + βx + γ = 0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(250, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 28);
+            this.label6.TabIndex = 8;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(35, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(21, 358);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(182, 19);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Δείχνε τις ρίζες της εξίσωσης";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 439);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vita);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.alpha);
+            this.Name = "Form1";
+            this.Text = "Quadratic equation solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox alpha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox vita;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox gama;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuadSolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label5.Text = "αx² + βx + γ = 0";
+                int a = int.Parse(alpha.Text);
+                int b = int.Parse(vita.Text);
+                int g = int.Parse(gama.Text);
+
+
+                float x = 0;
+                float x1 = 0;
+                float x2 = 0;
+
+                string _a = "";
+                string _b = "";
+                string _g = "";
+                if (a < 0)
+                {
+                    _a = "(" + a + ")";
+                    if (a == -1)
+                    {
+                        label5.Text = label5.Text.Replace("α", "-");
+                    }
+                    else
+                    {
+                        label5.Text = label5.Text.Replace("α", a.ToString());
+                    }
+                }
+                else
+                {
+                    _a = a.ToString();
+                    if(a == 1)
+                    {
+                        label5.Text = label5.Text.Replace("α", "");
+                    }
+                    else
+                    {
+                        label5.Text = label5.Text.Replace("α", a.ToString());
+                    }
+
+
+                }
+                
+                if (b < 0)
+                {
+                    _b = "(" + b + ")";
+                    label5.Text = label5.Text.Replace("+ β", b.ToString().Replace("-", "- "));
+                }
+                else
+                {
+                    _b = b.ToString();
+                    label5.Text = label5.Text.Replace("β", b.ToString());
+                }
+                if (g < 0)
+                {
+                    _g = "(" + g + ")";
+                    label5.Text = label5.Text.Replace("+ γ", g.ToString().Replace("-", "- "));
+
+                }
+                else
+                {
+                    _g = g.ToString();
+                    label5.Text = label5.Text.Replace("γ", g.ToString());
+                }
+
+                label5.Text = label5.Text.Replace(" 1x", " x");
+
+                int D = (int)Math.Pow(b, 2) - (4 * a * g);
+                label6.Text = "      Δ = " + _b + "² - 4 * " + _a + " * " + _g + "\n" + "=> Δ = " + D + "\n";
+                if (D > 0)
+                {
+                    label6.Text = label6.Text + "=> √Δ = " + Math.Sqrt(D);
+                }
+                label6.Text = label6.Text + "\n\n";
+
+                if (checkBox1.Checked == true)
+                {
+                    if (D > 0)
+                    {
+                        x1 = (float)((double)(b * (-1) + Math.Sqrt(D)) / (2 * a));
+                        x2 = (float)((double)(b * (-1) - Math.Sqrt(D)) / (2 * a));
+                        x1 = (float)System.Math.Round(x1, 4);
+                        x2 = (float)System.Math.Round(x2, 4);
+                        label6.Text = label6.Text + "x1 = (" + b * (-1) + " + " + (float)System.Math.Round(Math.Sqrt(D), 4) + ") / " + 2 * a + " => x1 = " + x1;
+                        label6.Text = label6.Text + "\n";
+                        label6.Text = label6.Text + "x2 = (" + b * (-1) + " - " + (float)System.Math.Round(Math.Sqrt(D), 4) + ") / " + 2 * a + " => x2 = " + x2;
+
+                    }
+
+                    if (D == 0)
+                    {
+                        x = (float)((double)(b * (-1) / (2 * a)));
+                        label6.Text = label6.Text + "x = " + x;
+                    }
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Invalid values. Please enter valid integers.", "Error");
+            }
+            
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
